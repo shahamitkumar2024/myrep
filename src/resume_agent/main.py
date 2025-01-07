4e1b9c45034c4a47b0bd6666a92fdff8002adf0c
shahamitkumar2024 <shahamitkumar@gmail.com>
2025-01-07 15:16:41 +0000

    test

--- src/resume_agent/main.py
+++ src/resume_agent/main.py
@@ -12,78 +12,12 @@ warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 # interpolate any tasks and agents information
 
 def run():
-    """
-    Run the crew.
-    """
-    
-    job_application_inputs = {
-    "job_posting_url": "https://jobs.lever.co/AIFund/6c82e23e-d954-4dd8-a734-c0c2c5ee00f1?lever-origin=applied&lever-source%5B%5D=AI+Fund",
-    "github_url": "https://github.com/joaomdmoura",
-    "personal_writeup": """Noah is an accomplished Software
-    Engineering Leader with 18 years of experience, specializing in
-    managing remote and in-office teams, and expert in multiple
-    programming languages and frameworks. He holds an MBA and a strong
-    background in AI and data science. Noah has successfully led
-    major tech initiatives and startups, proving his ability to drive
-    innovation and growth in the tech industry. Ideal for leadership
-    roles that require a strategic and innovative approach."""
-    }
-    ResumeAgent().crew().kickoff(inputs=job_application_inputs)
-
-
-
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    
-    job_application_inputs = {
-    "job_posting_url": "https://jobs.lever.co/AIFund/6c82e23e-d954-4dd8-a734-c0c2c5ee00f1?lever-origin=applied&lever-source%5B%5D=AI+Fund",
-    "github_url": "https://github.com/joaomdmoura",
-    "personal_writeup": """Noah is an accomplished Software
-    Engineering Leader with 18 years of experience, specializing in
-    managing remote and in-office teams, and expert in multiple
-    programming languages and frameworks. He holds an MBA and a strong
-    background in AI and data science. Noah has successfully led
-    major tech initiatives and startups, proving his ability to drive
-    innovation and growth in the tech industry. Ideal for leadership
-    roles that require a strategic and innovative approach."""
-    }
-    
-    try:
-        ResumeAgent().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=job_application_inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        ResumeAgent().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    job_application_inputs = {
-   "job_posting_url": "https://jobs.lever.co/AIFund/6c82e23e-d954-4dd8-a734-c0c2c5ee00f1?lever-origin=applied&lever-source%5B%5D=AI+Fund",
-    "github_url": "https://github.com/joaomdmoura",
-    "personal_writeup": """Noah is an accomplished Software
-    Engineering Leader with 18 years of experience, specializing in
-    managing remote and in-office teams, and expert in multiple
-    programming languages and frameworks. He holds an MBA and a strong
-    background in AI and data science. Noah has successfully led
-    major tech initiatives and startups, proving his ability to drive
-    innovation and growth in the tech industry. Ideal for leadership
-    roles that require a strategic and innovative approach."""
-    }
-    try:
-        ResumeAgent().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=job_application_inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
+            """
+            Run the crew.
+            """
+            
+            inputs = { 'job_posting_url': 'https://jobs.lever.co/AIFund/6c82e23e-d954-4dd8-a734-c0c2c5ee00f1?lever-origin=applied&lever-source%5B%5D=AI+Fund'}
+            try:
+                ResumeAgent().crew().kickoff(inputs=inputs)
+            except Exception as e:
+                raise Exception("An error occurred while running the crew: {e}")
